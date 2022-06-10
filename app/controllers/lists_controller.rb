1fe8d35259d4152ad5255cdd430afaf133fae77c
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-06-10 15:08:38 +0000

    [Finish]3-4-5-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,11 +2,11 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -14,13 +14,23 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  private
   
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
-  
+
 end
